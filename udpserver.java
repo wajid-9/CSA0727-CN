31bf16bf26270ed0d4f9c3e5c6cd3b0bbbd3a80b
wajid-9 <124772927+wajid-9@users.noreply.github.com>
2023-06-10 07:54:25 +0530

    Add files via upload

---
+++ udpserver.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.net.*;
+ 
+class UDPServer
+{
+   public static void main(String args[]) throws Exception
+      {
+         DatagramSocket serverSocket = new DatagramSocket(9876);
+  //Server Socekt Created
+ 
+            byte[] receiveData = new byte[1024];
+            byte[] sendData = new byte[1024];
+            while(true)
+               {
+                  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                  serverSocket.receive(receivePacket);
+                  String sentence = new String( receivePacket.getData());
+                  System.out.println("RECEIVED: " + sentence);
+ 
+                  InetAddress IPAddress = receivePacket.getAddress();
+                  int port = receivePacket.getPort();
+ 
+                  String capitalizedSentence = sentence.toUpperCase();
+   //Change sentence to Capital letter
+                  sendData = capitalizedSentence.getBytes();
+ 
+                  DatagramPacket sendPacket =
+                  new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                  serverSocket.send(sendPacket);
+   //Send Capitalized data back to client
+               }
+      }
+}
